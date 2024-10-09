566362e755b7be04b5bf6e0b00ff6ed3366b496e
Karthik Gnanakumar <karthikgappiah@gmail.com>
2024-10-09 00:50:25 -0700

    feat: structured aside

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "./components/shadcn/resizable";
+import { Skeleton } from "./components/shadcn/skeleton";
 
 interface AppProps {}
 
@@ -11,8 +12,21 @@ const App: FC<AppProps> = () => {
   return (
     <main className="flex h-screen flex-col">
       <ResizablePanelGroup direction="horizontal" className="grow">
-        <ResizablePanel className="bg-red-700">
-          {"TODO: <Aside />"}
+        <ResizablePanel className="">
+          <aside className="h-full overflow-y-auto">
+            {[...Array(100)].map((_, i) => (
+              <div
+                key={i}
+                className="flex items-center space-x-4 p-2 hover:bg-primary"
+              >
+                <Skeleton className="h-12 w-12 rounded-lg" />
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-64" />
+                  <Skeleton className="h-4 w-52" />
+                </div>
+              </div>
+            ))}
+          </aside>
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel className="bg-blue-700">
